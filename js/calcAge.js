ef3429a0d81f0c6c7617e49a965f9f0f6fc72176
yoooel <72613221+yoooel@users.noreply.github.com>
2021-04-17 17:35:51 +0000

    responsiveness added

--- js/calcAge.js
+++ js/calcAge.js
@@ -5,14 +5,17 @@ let ageAtDate = document.getElementById("age-at-date-of").value;
 let age = document.getElementById("age"); 
 // Getting the days, years and months for the date of birth calendar 
 let newDateOfBirthY = new Date(dateOfBirth);
+// 1995
 
 // Getting the days, years and months for the age at  date of  calendar 
 let newAgeOfDateY = new Date(ageAtDate);
+// 2021
 // Calculating the Age difference 
 let ageDiff = Math.abs(newAgeOfDateY - newDateOfBirthY);
-let show = ageDiff = ageDiff / 3.1536E+10;
+let show  = ageDiff / 3.1536E+10;
+// returns back to years-26 
 age.innerText = "Your age is : " + Math.floor(show);
-console.log(ageDiff);
+// console.log(ageDiff);
 }
 
 let findAge = document.getElementById("find-age");
